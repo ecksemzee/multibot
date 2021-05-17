0c4e8e3a45510911db8e1198c5baa684344f07c2
xms <45754441+ecksemzee@users.noreply.github.com>
2021-05-17 22:07:20 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// imports
+const fs = require("fs");
+const { BotSettings } = require("../config.json");
+const { Worker, isMainThread } = require("worker_threads");
+
+// read tokens
+function readTokens() {
+    const file = BotSettings.TokensFile;
+    try {
+        return fs.readFileSync(`../${file}`, {
+            encoding: "utf-8",
+            flag: "r"
+        })
+            .replace(/\r\n/g,'\n')
+            .split('\n')
+            .filter(
+                function(line) {
+                    if (line !== "")
+                        return line;
+                }
+            );
+    } catch(e) {
+        console.log(`Had a problem reading tokens file! Make sure there is '${file}' exists!`); // best documentation ever, without specifying path
+        process.exit();
+    }
+}
+
+// main sequence 
+console.log("Hold on, reading tokens");
+var tokens = readTokens();
+if (isMainThread) {
+    console.log("Done! Now attempting to create workers and make workers log in.");
+    for (var i = 0; i < tokens.length; i++) {
+        new Worker("./src/bot.js", {
+            argv:[
+                tokens[i], 
+                BotSettings.BotPrefix,
+                i + 1
+            ]
+        });
+    }
+}
